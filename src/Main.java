ced1e26d015f7c3f40b2af328062a952c54c5062
kylewebb16 <129976977+kylewebb16@users.noreply.github.com>
2024-11-15 08:52:00 -0500

    Start basic JFrame @src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
+        GUI(); 
+        
         BST myBst = new BST();
         myBst.insert(10);
         myBst.insert(5);
@@ -8,17 +13,36 @@ public class Main {
         myBst.insert(3);
         myBst.insert(1);
         myBst.insert(9);
-
+        
         System.out.println("\nIn order:");
         myBst.inOrderPrint();
-
+        
         System.out.println("\nPre order:");
         myBst.preOrderPrint();
-
+        
         System.out.println("\nPost order:");
         myBst.postOrderPrint();
+    }
 
-        myBst.deleteNode(10);
-        System.out.println(myBst.getRoot().value);
+
+    public static void GUI() {
+        // General pane properties
+        JFrame frame = new JFrame("Binary Search Tree");
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon image = new ImageIcon("../images/binary-tree.png");
+        frame.setIconImage(image.getImage());
+        // frame.setLayout();
+
+        // JPanel pane = new JPanel(new GridBagLayout());
+        
+        
+        // Label properties
+        
+        // Adding components to the frame
+        
+        
+        frame.setVisible(true);
     }
 }
